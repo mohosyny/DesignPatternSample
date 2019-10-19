d1601981ec9876005e492fe6eeb56a000c5229ac
mohosyny <mohosyny@gmail.com>
2019-10-19 23:46:38 +0330

    Add Square.class

---
+++ app/src/main/java/neo/mohosyny/designpatterns/Factory/Square.java
@@ -0,0 +1,12 @@
+package neo.mohosyny.designpatterns.Factory;
+
+/**
+ * Created by user on 2/18/2018.
+ */
+
+public class Square implements Shape {
+    @Override
+    public void draw() {
+
+    }
+}
