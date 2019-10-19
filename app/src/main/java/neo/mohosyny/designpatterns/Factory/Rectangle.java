0d667c7d0a9032a292b83afc19b07a95af1b41ba
mohosyny <mohosyny@gmail.com>
2019-10-19 23:45:29 +0330

    Add Rectangle.class

---
+++ app/src/main/java/neo/mohosyny/designpatterns/Factory/Rectangle.java
@@ -0,0 +1,12 @@
+package neo.mohosyny.designpatterns.Factory;
+
+/**
+ * Created by user on 2/18/2018.
+ */
+
+public class Rectangle implements Shape {
+    @Override
+    public void draw() {
+
+    }
+}
