ca4c6e0bdc829e231270f09719b7206288214900
mohosyny <mohosyny@gmail.com>
2019-10-19 23:50:17 +0330

    Add Factory usage  in MainActivity

--- app/src/main/java/neo/mohosyny/designpatterns/MainActivity.java
+++ app/src/main/java/neo/mohosyny/designpatterns/MainActivity.java
@@ -5,6 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import neo.mohosyny.designpatterns.Builder.Person;
+import neo.mohosyny.designpatterns.Factory.Shape;
+import neo.mohosyny.designpatterns.Factory.ShapeFactory;
+import neo.mohosyny.designpatterns.Factory.Square;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,9 +21,15 @@ public class MainActivity extends AppCompatActivity {
                 .setAge(26)
                 .setWeight(80)
                 .setHeight(175)
-                .setFirstName("moho")
+                .setFirstName("mo")
                 .setLastName("hsyny")
                 .Build();
+
+
+
+        //Factory pattern usage
+        Shape circle = ShapeFactory.getShape("Square");
+
 
     }
 }
